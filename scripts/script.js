1149aed298ec490f64f0eba5b563672d6c5e1575
Tin_Le <jackletrungtin@gmail.com>
2022-11-15 09:44:48 -0800

    added 3 ways of doing function

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+function name_name(str){
+    console.log(`Hallo ${str}`);
+}
+
+name_name("buckaroo");
+
+let greet = function(userName){
+    console.log("YOU GOT " + userName);
+}
+
+greet("MAIL");
+
+let hello = (nameUser) => {
+    console.log("WELCOME " + nameUser);
+}
+
+hello("eRIC")
