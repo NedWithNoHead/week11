4e18019c2eab8b439bbe049ffd1d2a48f765d0f6
Tin_Le <jackletrungtin@gmail.com>
2022-11-15 10:41:58 -0800

    populate a list with elements

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
-function name_name(str){
-    console.log(`Hallo ${str}`);
-}
+// function name_name(str,time = "Day"){
+//     console.log(`Hallo ${time} ${str}`);
+// }
 
-name_name("buckaroo");
+// name_name("buckaroo", "night");
 
 let greet = function(userName){
     console.log("YOU GOT " + userName);
@@ -15,3 +15,29 @@ let hello = (nameUser) => {
 }
 
 hello("eRIC")
+
+function name_name(str,time = "Day"){
+    return(`Hallo ${time} ${str}`);
+}
+
+const results = name_name("guy", "day");
+console.log(results);
+
+let shoppingList = ["chesse", "bread", "ham"]
+
+const ul_list = document.querySelector(".shopping"); 
+function populate(list){
+    // alist.forEach(item){
+    // item = `<li>${item}</li>`,
+    // console. log (item);
+    // }
+    for (let i = 0; i < list.length; i++)
+    {
+        console.log(list[i]);
+        const ListItemElement = document.createElement("li");
+        ListItemElement.textContent = list[i];
+        ul_list.append(ListItemElement);
+
+    }
+}
+populate(shoppingList)
